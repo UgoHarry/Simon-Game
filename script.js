73385b600df28b5d8f9f93fab29f6ae8d748f897
Ugo Anekwe <harryphoenix99@gmail.com>
2018-02-27 05:26:59 +0000

    Fixing timing issue

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-// function tryy() {
-//   var test = document.getElementById("powerSwitch");
-//   if (test.checked === true) {
-//     console.log("On");
-//   }
-//   else {
-//     console.log("Off");
-//   }
-// }
+function tryy() {
+  var test = document.getElementById("powerSwitch");
+  if (test.checked === true) {
+    console.log("On");
+    console.log(test.checked);
+    startGame();
+  }
+  else {
+    console.log("Off");
+    console.log(test.checked);
+  }
+}
 
-// tryy();
+
+
+init();
+
 
 var activePlayer, testPattern;
 
 function init() {
   // set counter field to zero
   document.getElementById("counter").setAttribute("value", "0");
-
   // create variable for score
   let score = 0;
-  
   // create empty array variable for pattern
   testPattern = [];
+  // tryy();
 }
 
-init();
+
 var index = 1;
 function startGame() {
   // create a random between 0-3
@@ -41,7 +46,7 @@ function startGame() {
    if(i == testPattern.length){
      clearInterval(myInterval);
    }
-  });
+  }, 1000);
 
   // var beep = document.getElementById("audio" + testPattern[0]);
   // beep.play();
@@ -56,9 +61,9 @@ function checkPlay() {
 // Add bright colour and sound
 function colorTune (id) {
   var quarter = document.getElementById(id);
-  quarter.className += "lighter";
+  quarter.classList.add("lighter");
   setTimeout (function(){
-    quarter.className -= "lighter";
+    quarter.classList.remove("lighter");
   }, 500)
   // var mySound = document.getElementById("audio" + id);
   // mySound.play();
