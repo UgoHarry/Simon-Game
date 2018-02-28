001b363a2c2005560defb2d0038b384a332f22a7
ugo.harry@gmail.com <ugo.harry@gmail.com>
2018-02-28 21:49:38 +1100

    Changed the look, added play tune function

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-function tryy() {
+
+init();
+
+var playerPattern, simonPattern;
+
+function init() {
+  // set counter field to zero
+  document.getElementById("counter").setAttribute("value", "0");
+  // create variable for score
+  let score = 0;
+  // create empty array variable for pattern
+  simonPattern = [];
+  playerPattern = [];
+  // tryy();
+}
+
+function start() {
   var test = document.getElementById("powerSwitch");
   if (test.checked === true) {
     console.log("On");
     console.log(test.checked);
-    startGame();
+    simonGame();
   }
   else {
     console.log("Off");
@@ -13,48 +29,37 @@ function tryy() {
 
 
 
-init();
-
-
-var activePlayer, testPattern;
-
-function init() {
-  // set counter field to zero
-  document.getElementById("counter").setAttribute("value", "0");
-  // create variable for score
-  let score = 0;
-  // create empty array variable for pattern
-  testPattern = [];
-  // tryy();
-}
-
-
 var index = 1;
-function startGame() {
+function simonGame() {
   // create a random between 0-3
   let randomPick = Math.floor(Math.random() * 4);
-  // Push number to testPattern array
-  testPattern.push(randomPick);
-  console.log("TestPattern");
-  console.log(testPattern);
+  // Push number to simonPattern array
+  simonPattern.push(randomPick);
+  console.log("Simon Pattern");
+  console.log(simonPattern);
   var i = 0;
 
   var myInterval = setInterval(function() {
-   var pos = testPattern[i];
+   var pos = simonPattern[i];
    colorTune(pos);
    i++;
-   if(i == testPattern.length){
+   if(i == simonPattern.length){
      clearInterval(myInterval);
    }
   }, 1000);
 
   // var beep = document.getElementById("audio" + testPattern[0]);
   // beep.play();
-
-
 }
 
-function checkPlay() {
+//Function that runs on every user play
+function userPlay(id) {
+  console.log(id);
+  // Adding pad id to player pattern array
+  playerPattern.push(id * 1);
+  console.log(playerPattern);
+  colorTune(id*1);
+  
 }
 
 
@@ -62,9 +67,16 @@ function checkPlay() {
 function colorTune (id) {
   var quarter = document.getElementById(id);
   quarter.classList.add("lighter");
+  playTune(id);
   setTimeout (function(){
     quarter.classList.remove("lighter");
   }, 500)
+
+
+function playTune (id) {
+  var mySound = document.getElementById("audio" + id);
+  mySound.play();
+}
   // var mySound = document.getElementById("audio" + id);
   // mySound.play();
 }
